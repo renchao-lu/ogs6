ddb873290f4bd0d9c10348259f21ed9dd09870f4
renchao_lu <renchao.lu@gmail.com>
2020-01-09 10:27:58 +0100

    wip.

--- ChemistryLib/PhreeqcKernelData/Surface.h
+++ ChemistryLib/PhreeqcKernelData/Surface.h
@@ -47,6 +47,28 @@ class Surface final : private cxxSurface
 public:
     Surface(std::vector<SurfaceComponent> surface_components,
             std::vector<SurfaceCharge> surface_charges_);
+
+    void setChemicalSystemID(std::size_t const chemical_system_id)
+    {
+        Set_n_user_both(chemical_system_id);
+    }
+
+    void equilibrateWithSolution(bool const flag,
+                                 std::size_t const chemical_system_id)
+    {
+        solution_equilibria = true;
+        n_solution = chemical_system_id;
+    }
+
+    cxxSurface* castToBaseClass()
+    {
+        return static_cast<cxxSurface*>(this);
+    }
+
+    void sort()
+    {
+        Sort_comps();
+    }
 };
 }  // namespace PhreeqcKernelData
 }  // namespace ChemistryLib
